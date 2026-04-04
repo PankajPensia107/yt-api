5b79b2053aebc9afbe26e3eba2015bb68a871ae5
Developer Pankaj <167018220+PankajPensia107@users.noreply.github.com>
2026-04-04 15:39:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,25 @@
 from fastapi import FastAPI
-from youtube_transcript_api import YouTubeTranscriptApi, TranscriptsDisabled, NoTranscriptFound
+from youtube_transcript_api import YouTubeTranscriptApi
 
 app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "API running"}
 
 @app.get("/summary")
 def get_summary(video_id: str):
     try:
-        transcript = YouTubeTranscriptApi.get_transcript(video_id)
-        text = " ".join([x['text'] for x in transcript])
+        api = YouTubeTranscriptApi()
+
+        transcript = api.fetch(video_id)
+
+        text = " ".join([x.text for x in transcript])
 
         return {
             "video_id": video_id,
             "summary": text[:500]
         }
-
-    except TranscriptsDisabled:
-        return {"error": "Transcripts are disabled for this video"}
-
-    except NoTranscriptFound:
-        return {"error": "No transcript found for this video"}
 
     except Exception as e:
         return {"error": str(e)}
